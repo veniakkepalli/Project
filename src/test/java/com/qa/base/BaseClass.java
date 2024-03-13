e2a44d86f95b5effca71859cb9d82af4d78045dd
veniakkepalli <akkepalliveni@gmail.com>
2024-03-13 16:43:52 +0530

    second commit

--- src/test/java/com/qa/base/BaseClass.java
+++ src/test/java/com/qa/base/BaseClass.java
@@ -6,9 +6,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
